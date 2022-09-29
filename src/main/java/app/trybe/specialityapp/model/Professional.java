0bc430ed55484339357a6189762f614209660a67
felipemalli <felipevahia@gmail.com>
2022-09-29 17:27:07 -0300

    feat: implement Professional model

---
+++ src/main/java/app/trybe/specialityapp/model/Professional.java
@@ -0,0 +1,57 @@
+package app.trybe.specialityapp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Entidade Professional.
+ */
+@Entity
+public class Professional {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Integer id;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "speciality")
+  private String speciality;
+
+  /**
+   * Professional Constructor.
+   */
+  public Professional(Integer id, String name, String speciality) {
+    this.id = id;
+    this.name = name;
+    this.speciality = speciality;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSpeciality() {
+    return speciality;
+  }
+
+  public void setSpeciality(String speciality) {
+    this.speciality = speciality;
+  }
+}
