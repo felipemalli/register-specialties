fdccc94df16073752d41b3a5eacbc2e44ffb6e53
felipemalli <felipevahia@gmail.com>
2022-09-29 18:12:37 -0300

    feat: implement ApplicationError class

---
+++ src/main/java/app/trybe/specialityapp/commons/ApplicationError.java
@@ -0,0 +1,33 @@
+package app.trybe.specialityapp.commons;
+
+import javax.ws.rs.core.Response.Status;
+
+/**
+ * ApplicationError class.
+ */
+public class ApplicationError extends RuntimeException {
+  private Status status;
+  private String message;
+
+  public ApplicationError(Status status, String message) {
+    this.status = status;
+    this.message = message;
+  }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public void setStatus(Status status) {
+    this.status = status;
+  }
+
+  @Override
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+}
